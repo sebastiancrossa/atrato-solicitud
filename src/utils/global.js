5a17c4b3f3e88000fa54fb37175de0dc60d8905b
Sebastian Crossa <crossasebastian@gmail.com>
2019-06-04 15:24:46 -0600

    Added the basic styles to a theme

---
+++ src/utils/global.js
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+        margin: 0;
+        padding: 0;
+        box-sizing: inherit;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
+    html {
+        font-size: 62.5%;
+        /* 1rem = 10px */
+
+        box-sizing: border-box;
+
+        /* Declaracion de nuestras variables CSS */
+        --color-main: ${props => props.theme.colors.main};
+        --color-text: ${props => props.theme.colors.textColor};
+        --color-shadow: ${props => props.theme.colors.shadow};
+
+        @media ${props => props.theme.mediaQueries.small} {
+            font-size: 80%;
+        }
+
+        @media ${props => props.theme.mediaQueries.smallest} {
+            font-size: 60%;
+        }
+    }
+
+    body {
+        font-family: 'Raleway', sans-serif;
+        font-weight: 400;
+        font-height: 1.6;
+    }
+
+    a,
+    input,
+    textarea,
+    button {
+        outline: none;
+        text-decoraton: none;
+        font-family: inherit;
+    }
+`;
---
+++ src/utils/theme.js
@@ -0,0 +1,17 @@
+const theme = {
+  colors: {
+    main: '#03B0F0',
+    textColor: '#19191D',
+    shadow: 'rgba(232, 236, 251, 88)'
+  },
+
+  mediaQueries: {
+    smallest: `only screen and (max-width: 25em)`,
+    small: `only screen and (max-width: 37.5em)`,
+    medium: `only screen and (max-width: 56em)`,
+    large: `only screen and (max-width: 80em)`,
+    largest: `only screen and (max-width: 90em)`
+  }
+};
+
+export default theme;
